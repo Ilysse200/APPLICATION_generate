16a169fe58277e79d20e7d650c3410acdeccceeb
Ilysse200 <ilysseted500@gmail.com>
2025-03-06 15:05:19 +0200

    added initial sidebar with it's styles

--- generatingApplicationsProject/src/DashboardMaterial/Sidebar.jsx
+++ generatingApplicationsProject/src/DashboardMaterial/Sidebar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { FaChartBar, FaUser, FaCog, FaSignOutAlt, FaClipboardList } from "react-icons/fa";
+import { MdAnalytics } from "react-icons/md";
+import { IoIosApps } from "react-icons/io";
+import "./dashboardStyles/sidebar.css"; // Import the CSS file
 
-function Sidebar() {
+const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <ul className="sidebar-menu">
+        <li className="sidebar-item">
+          <FaChartBar className="sidebar-icon" />
+          <span>Dashboard</span>
+        </li>
+        <li className="sidebar-item">
+          <FaClipboardList className="sidebar-icon" />
+          <span>Forms</span>
+        </li>
+        <li className="sidebar-item">
+          <IoIosApps className="sidebar-icon" />
+          <span>Applications</span>
+        </li>
+        <li className="sidebar-item">
+          <MdAnalytics className="sidebar-icon" />
+          <span>Analytics</span>
+        </li>
+        <li className="sidebar-item active">
+          <FaUser className="sidebar-icon" />
+          <span>Users</span>
+        </li>
+        <li className="sidebar-item">
+          <FaCog className="sidebar-icon" />
+          <span>Settings</span>
+        </li>
+        <li className="sidebar-item">
+          <FaSignOutAlt className="sidebar-icon" />
+          <span>Logout</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
