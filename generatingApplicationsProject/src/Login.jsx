55e866ca683bc8406f24ac85e178cdb5fde83050
Ilysse200 <ilysseted500@gmail.com>
2025-03-27 13:08:41 +0200

    registered user

--- generatingApplicationsProject/src/Login.jsx
+++ generatingApplicationsProject/src/Login.jsx
@@ -1,34 +1,90 @@
-import React from 'react'
-import './styles/login.css'
-function Login({handleRegister}) {
-    // // create usestates that will send you back to the login/register forms
+import React, { useState } from 'react';
+import axios from 'axios';
+import './styles/login.css';
 
-    // const[isLogin, setIsLogin] = useState(false);
-    // const handleLogin = () => {
-    //     setIsLogin(!isLogin);
-    // }
+{/*Import icons for registration */}
+import { IoIosClose } from "react-icons/io";
+
+function Login({ handleRegister }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [successMessage, setSuccessMessage] = useState(false);
+
+  const handleRegisterClick = async () => {
+    try {
+      const response = await axios.post('http://localhost:5009/users/register', {
+        name: username,
+        password: password,
+        userRole: "User"
+      });
+
+      if (response.data) {
+        setSuccessMessage(true);
+        setTimeout(() => {
+          setSuccessMessage(false);
+          handleRegister(); 
+      }, 3000);
+        // ✅ Flip to login view
+      }
+    } catch (error) {
+      console.error('Registration failed:', error.response?.data || error.message);
+    }
+  };
+
   return (
-    <div className='container-register'>     
-        <div className='LoginPage'>
-            <div className='entire-form'>
-            <h3>Register Account</h3>
-            {/* {isLogin && <SignUp handleLogin={handleLogin}/>} */}
-            <form className='register-form'>
-                <label className='username-text'>Username</label>
-                <input type='text' placeholder='Enter username'className='username-holder'></input>
-                <label className='username-text'>Password</label>
-                <input type='text' placeholder='Enter password'className='password-holder'></input>
-                <button type='button' className='register-button'>Create Account</button>
-                <p className='password-recover'>Forget password?</p>
-                <p className='last-line'>Already have an account?
-                    <span className='register-path' onClick={handleRegister}>Login</span>
-                    </p>
-            </form>   
-            </div>
-            
+    <div className='container-register'>
+      {/* Success Pop-up Message */}
+      {successMessage && (
+                <div className="success-popup">
+                    <div className="success-content">
+                        ✅ User registered Successfully
+                        <button className="close-btn" onClick={() => setSuccessMessage(false)}><IoIosClose className='close-btn-success'/></button>
+                    </div>
+                    <div className="success-timer"></div>
+                </div>
+            )}
+      <div className='LoginPage'>
+        <div className='entire-form'>
+          <h3>Register Account</h3>
+          <form className='register-form' onSubmit={(e) => e.preventDefault()}>
+            <label className='username-text'>Username</label>
+            <input
+              type='text'
+              placeholder='Enter username'
+              className='username-holder'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <label className='username-text'>Password</label>
+            <input
+              type='password'
+              placeholder='Enter password'
+              className='password-holder'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button
+              type='button'
+              className='register-button'
+              onClick={handleRegisterClick}
+            >
+              Create Account
+            </button>
+
+            <p className='password-recover'>Forget password?</p>
+            <p className='last-line'>
+              Already have an account?
+              <span className='register-path' onClick={handleRegister}>
+                Login
+              </span>
+            </p>
+          </form>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
